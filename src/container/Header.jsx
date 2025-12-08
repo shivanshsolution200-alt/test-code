63577d3cd0ce591f445978a43155f52fc0d81a13
dxgajera1612-lang <dxgajera1612@gmail.com>
2025-12-08 13:41:15 +0530

    yes

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import meeshologo from "../assets/Meesho_logo.png";
+import meeshologo from "../images/meeshoLogo.svg";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LazyImage from "../components/LazyImage";
