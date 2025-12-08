19b889455fbf0bbf2568c95b1b6f21dee0c61faa
dxgajera1612-lang <dxgajera1612@gmail.com>
2025-12-08 12:19:21 +0530

    yes

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import homepagebannergif from "../images/pngmeesho.jpg";
 import bomb from "../images/bomb.png";
-import thustedimg from "../images/thrustedimg62673a64c85253f85502.png";
+import thustedimg from "../images/thrustedimg.png";
 import { Autoplay } from "swiper/modules";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
